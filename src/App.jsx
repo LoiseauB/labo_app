ef4d38e612bb0b858434e5f7688f324ceee695ca
LoiseauB <bapt.loiseau@gmail.com>
2023-06-22 12:21:56 +0200

    test without base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
