580feccfec2d351fc31a9f81d5567625b050eeaf
LoiseauB <bapt.loiseau@gmail.com>
2023-06-23 16:51:33 +0200

    change dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Markdown from 'vite-plugin-react-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({include: [/\.tsx$/, /\.md$/],}), Markdown({}),],
+  plugins: [react({include: [/\.tsx$/, /\.md$/],}),],
   base:'/labo_app/',
   assetsInclude:['**/*.md'],
 })
